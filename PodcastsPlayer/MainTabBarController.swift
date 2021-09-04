1f9d8b550a5bfa223ad4458f309bd4d72d9fed76
Stas <staszhingel@icloud.com>
2021-09-04 11:55:38 +0300

    Navigation

---
+++ PodcastsPlayer/MainTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabBarController.swift
+//  PodcastsPlayer
+//
+//  Created by Стас Жингель on 03.09.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UINavigationBar.appearance().prefersLargeTitles = true
+        tabBar.tintColor = .purple
+        
+        viewControllers = [generationnavigationController(with: ViewController(), image: UIImage(named: "favorites")! , title: "Favorites"),
+                           generationnavigationController(with: ViewController(), image: UIImage(named: "search")!, title: "Search"),
+                           generationnavigationController(with: ViewController(), image: UIImage(named: "downloads")!, title: "Downloads")]
+    }
+    
+    
+    
+   fileprivate func generationnavigationController(with rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        let navigationViewController = UINavigationController(rootViewController: rootViewController)
+    rootViewController.title = title
+    navigationViewController.tabBarItem.title = title
+    navigationViewController.tabBarItem.image = image
+    return navigationViewController
+    }
+}

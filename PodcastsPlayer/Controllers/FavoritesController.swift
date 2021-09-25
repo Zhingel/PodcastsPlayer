cf3f057d9c3e578fa78aae0a99e30917b30980b1
Stas <staszhingel@icloud.com>
2021-09-25 18:02:52 +0300

    collectionViewcell in favorites

--- PodcastsPlayer/Controllers/FavoritesController.swift
+++ PodcastsPlayer/Controllers/FavoritesController.swift
@@ -17,7 +17,7 @@ class FavoritesController: UICollectionViewController, UICollectionViewDelegateF
     fileprivate let cellId = "cellId"
     fileprivate func setupCollectionView() {
         collectionView.backgroundColor = .white
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(FavoritesPodcastCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     //MARK: - line spacing and delegate methods
@@ -27,13 +27,12 @@ class FavoritesController: UICollectionViewController, UICollectionViewDelegateF
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId , for: indexPath)
-        cell.backgroundColor = .gray
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = (view.frame.width - 3 * 16)/2
-        return CGSize(width: width, height: width )
+        return CGSize(width: width, height: width + 46)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
---
+++ PodcastsPlayer/Views/FavoritesPodcastCell.swift
@@ -0,0 +1,43 @@
+//
+//  FavoritesPodcastCell.swift
+//  PodcastsPlayer
+//
+//  Created by Стас Жингель on 25.09.2021.
+//
+
+import UIKit
+
+class FavoritesPodcastCell: UICollectionViewCell {
+    let imageView = UIImageView(image: UIImage(named: "appicon"))
+    let nameLabel = UILabel()
+    let artistNameLabel = UILabel()
+    fileprivate func stylizeUI() {
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+        nameLabel.text = "Podcast layout"
+        nameLabel.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        artistNameLabel.text = "artist layout"
+        artistNameLabel.font = UIFont.systemFont(ofSize: 14)
+        artistNameLabel.textColor = .lightGray
+    }
+    
+    fileprivate func setupViews() {
+        let stackView = UIStackView(arrangedSubviews: [imageView,nameLabel,artistNameLabel])
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        stackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        stylizeUI()
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
